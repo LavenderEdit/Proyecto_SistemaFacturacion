06f3bbdec77020ef8f82f7fc8358e1d791cf0c5b
Joan Lavender <alcachofasnews432@gmail.com>
2024-09-10 15:21:00 -0500

    CambioMartes

--- JS/scriptBillApp.js
+++ JS/scriptBillApp.js
@@ -1,9 +1,7 @@
 import {showOptionList} from './scriptsReutilizables.js';
 import {cambioPagina} from './scriptsFetch.js';
-import {setupModalCloseButtons} from './scriptMensajes.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     showOptionList();
     cambioPagina();
-    setupModalCloseButtons();
 });
--- JS/scriptsFetch.js
+++ JS/scriptsFetch.js
@@ -1,60 +1,68 @@
-import {billAppDatosFactura} from './scriptsDatosPaginas.js';
-import {showModal} from './scriptMensajes.js';
+import { billAppDatosFactura } from "./scriptsDatosPaginas.js";
+import { showModal, setupModalCloseButtons } from "./scriptMensajes.js";
+
+function cargarPagina(url) {
+  fetch(url)
+    .then((response) => response.text())
+    .then((html) => {
+      billAppDatosFactura.contentContainer.innerHTML = html;
+    })
+    .catch((error) => {
+      showModal("errorModal", error);
+    });
+}
 
 export function cambioPagina() {
-  billAppDatosFactura.buttons.forEach(boton => {
+  window.onload = function () {
+    setupModalCloseButtons();
+    cargarPagina("defaultPage.html");
+  };
+
+  billAppDatosFactura.buttons.forEach((boton) => {
     boton.addEventListener("click", function () {
       let url = "";
 
-      const buttonText = this.querySelector('span').innerText;
+      const buttonText = this.querySelector("span").innerText;
 
       switch (buttonText) {
         case "Realizar venta":
           url = "RealizarVenta.html";
-          showModal('successModal', "Exito");
+          showModal("successModal", "Exito");
           break;
         case "Ventas Realizadas":
           url = "VentasRealizadas.html";
-          showModal('successModal', "Exito");
+          showModal("successModal", "Exito");
           break;
         case "Guías de Remisión":
           url = "GuiaRemision.html";
-          showModal('successModal', "Exito");
+          showModal("successModal", "Exito");
           break;
         case "Proformas":
           url = "Proformas.html";
-          showModal('successModal', "Exito");
+          showModal("successModal", "Exito");
           break;
         case "Reportes":
           url = "Reportes.html";
-          showModal('successModal', "Exito");
+          showModal("successModal", "Exito");
           break;
         case "Clientes y Proveedores":
           url = "ClientesProveedores.html";
-          showModal('successModal', "Exito");
+          showModal("successModal", "Exito");
           break;
         case "Productos y Servicios":
           url = "ProductosServicios.html";
-          showModal('successModal', "Exito");
+          showModal("successModal", "Exito");
           break;
         case "Configuración":
           url = "Configuracion.html";
-          showModal('successModal', "Exito");
+          showModal("successModal", "Exito");
           break;
         default:
           url = "defaultPage.html";
           break;
       }
 
-      fetch(url)
-        .then(response => response.text())
-        .then(html => {
-          billAppDatosFactura.contentContainer.innerHTML = html;
-        })
-        .catch((error) => {
-            showModal('errorModal', error);
-        });
+      cargarPagina(url);
     });
   });
 }
-
