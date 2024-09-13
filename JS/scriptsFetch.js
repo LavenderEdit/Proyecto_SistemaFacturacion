0a842a24d6af5c2603bf4aa1c5cbb97a5ff24308
Joan Lavender <alcachofasnews432@gmail.com>
2024-09-12 19:51:02 -0500

    CambioJueves

--- JS/scriptsFetch.js
+++ JS/scriptsFetch.js
@@ -27,35 +27,27 @@ export function cambioPagina() {
       switch (buttonText) {
         case "Realizar venta":
           url = "RealizarVenta.html";
-          showModal("successModal", "Exito");
           break;
         case "Ventas Realizadas":
           url = "VentasRealizadas.html";
-          showModal("successModal", "Exito");
           break;
         case "Guías de Remisión":
           url = "GuiaRemision.html";
-          showModal("successModal", "Exito");
           break;
         case "Proformas":
           url = "Proformas.html";
-          showModal("successModal", "Exito");
           break;
         case "Reportes":
           url = "Reportes.html";
-          showModal("successModal", "Exito");
           break;
         case "Clientes y Proveedores":
           url = "ClientesProveedores.html";
-          showModal("successModal", "Exito");
           break;
         case "Productos y Servicios":
           url = "ProductosServicios.html";
-          showModal("successModal", "Exito");
           break;
         case "Configuración":
           url = "Configuracion.html";
-          showModal("successModal", "Exito");
           break;
         default:
           url = "defaultPage.html";
